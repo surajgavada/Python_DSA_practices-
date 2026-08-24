74152ead0a94436e8a8f324840e94059d47e20b4
surajgavada <surajgavada17@gmail.com>
2026-08-24 23:44:28 +0530

    f string

---
+++ day2/f-s.py
@@ -0,0 +1,6 @@
+name = input("enter you name")
+age = input("enter you age")
+yob = input("enter you yob")
+
+# print("yournasme is",name,"your age is",age,"your yob is",yob)
+print(f"your name is {name} your age is {age}")
