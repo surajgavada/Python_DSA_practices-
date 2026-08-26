04b31d52ebd55d5ab10cab7acf5f3a5f82e724b3
surajgavada <surajgavada17@gmail.com>
2026-08-26 22:28:01 +0530

    largest element in an array

--- day3/2.py
+++ day3/2.py
@@ -1,6 +1,15 @@
+# # #largest of Element in an array
+# num = [55,41,86,97,22,1,99]
+# larg = num[0]
+# n = len(num)
+# for i in range (0,n):
+#     larg = max(larg,num[i])
+# print (larg)
+
 # #largest of Element in an array
 num = [55,41,86,97,22,1,99]
-larg = num[0]
+larg = float("-inf")
+print(larg)
 n = len(num)
 for i in range (0,n):
     larg = max(larg,num[i])
