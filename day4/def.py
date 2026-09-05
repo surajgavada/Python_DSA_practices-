5c928beac7b05ad65b8cf09ed2e85fd514607248
surajgavada <surajgavada17@gmail.com>
2026-09-05 23:48:47 +0530

    ..

--- day4/def.py
+++ day4/def.py
@@ -1,20 +1,27 @@
-def rotation(num, k=3):
-    n = len(num)
-    # if n == 0:
-    #     return num
+# def rotation(num, k=3):
+#     n = len(num)
+#     # if n == 0:
+#     #     return num
 
-    r = k % n
-    for _ in range(r):
-        e = num.pop()
-        num.insert(0, e)
-    # return num
+#     r = k % n
+#     for _ in range(r):
+#         e = num.pop()
+#         num.insert(0, e)
+#     # return num
 
 
-num = []
-n = int(input("Enter the number of elements: "))
-for i in range(n):
-    arr = int(input(f"Element {i+1}: "))
-    num.append(arr)
+# num = []
+# n = int(input("Enter the number of elements: "))
+# for i in range(n):
+#     arr = int(input(f"Element {i+1}: "))
+#     num.append(arr)
 
-rotation(num)
-# print("Rotated list:", num)
+# rotation(num)
+# # print("Rotated list:", num)
+
+num = [5,6,8,1,2,4]
+k = 8
+n = len(num)
+k = k%n
+num[:] = num[n-k:]+num[:n-k]
+print(num)
